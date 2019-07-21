77eb642c0aac4afab32622f8b4b3b5a721eb70f7
Vuk Samardžić <samardzic.vuk@gmail.com>
2019-07-21 20:05:20 +0200

    feat: init markup w/ basic js structure

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import '../scss/main.scss';
+
+const arr = [1, 2, 3];
+const el = document.querySelector('body');
+
+
+arr.forEach((i, index) => {
+  // const n = document.createElement('p');
+  // n.append(`${i} - el`);
+  // el.appendChild(n);
+});
