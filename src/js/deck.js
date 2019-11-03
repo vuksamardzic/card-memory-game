e566c9c81b4928691e6adc34037fa1da0bf7aead
Vuk Samardžić <samardzic.vuk@gmail.com>
2019-11-03 19:14:10 +0100

    feat: card shapes, logic to randomize shapes from pool

--- src/js/deck.js
+++ src/js/deck.js
@@ -1,14 +1,38 @@
+let shape = [
+  `<i class="fa fa-heart" aria-hidden="true"></i>`,
+  `<i class="fa fa-bolt" aria-hidden="true"></i>`,
+  `<i class="fa fa-leaf" aria-hidden="true"></i>`,
+  `<i class="fa fa-paper-plane" aria-hidden="true"></i>`,
+  `<i class="fa fa-star" aria-hidden="true"></i>`,
+  `<i class="fa fa-plane" aria-hidden="true"></i>`,
+  `<i class="fa fa-eur" aria-hidden="true"></i>`,
+  `<i class="fa fa-bomb" aria-hidden="true"></i>`,
+  `<i class="fa fa-camera" aria-hidden="true"></i>`,
+  `<i class="fa fa-child" aria-hidden="true"></i>`,
+  `<i class="fa fa-cube" aria-hidden="true"></i>`,
+  `<i class="fa fa-eye" aria-hidden="true"></i>`,
+  `<i class="fa fa-key" aria-hidden="true"></i>`,
+  `<i class="fa fa-tint" aria-hidden="true"></i>`,
+  `<i class="fa fa-tree" aria-hidden="true"></i>`,
+  `<i class="fa fa-umbrella" aria-hidden="true"></i>`
+];
+
 export class Deck {
   constructor() {
     this.model = [];
   }
 
+
   generateDeck(len) {
+    shape = this.shuffleDeck(shape, 4);
+    let cut = shape.slice(0, len / 2);
+    cut = cut.concat(cut);
+    cut = this.shuffleDeck(cut, 4);
     let arr = [...Array(len).keys()].map((i, index) => {
       if (index % 2 !== 0) {
-        return { value: i - 1, open: false, match: false, id: index };
+        return { value: cut[i], open: false, match: false, id: index };
       }
-      return { value: i, open: false, match: false, id: index };
+      return { value: cut[i], open: false, match: false, id: index };
     });
     arr = this.shuffleDeck(arr, 4);
     return arr;
