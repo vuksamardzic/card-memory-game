358974c4f3d6c9f25a3bf65f671b641228fe457a
Vuk Samardžić <samardzic.vuk@gmail.com>
2019-11-03 19:14:31 +0100

    chore: clean index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,30 +2,4 @@ import '../scss/main.scss';
 import { UI } from './ui';
 
 const ui = new UI();
-
-ui.initGame();
-
-// const el = document.querySelector('#a');
-//
-// const waitForHello = ms => new Promise((r, j) => {
-//   const check = () => {
-//     console.log('checking', ms);
-//     if (el.innerHTML === 'Hello world')
-//       r();
-//     else if ((ms -= 100) < 0)
-//       j('promise rejected');
-//     else
-//       setTimeout(check, 100);
-//   };
-//   setTimeout(check, 100);
-// });
-
-// setTimeout(() => {
-//   el.innerHTML = 'Hello world';
-// }, 4500);
-
-// (async () => {
-//   el.innerHTML = 'waiting..';
-//   waitForHello(3000);
-// })();
-
+ui.init();
